2f42ebec82832e8f10bf281274dc378b349d2fa4
Sandra Rum <sandra@tartaros>
2014-01-27 10:43:27 +0100

    Pep8 and replaced long names by short ones.

--- src/inqbus/installer/installer.py
+++ src/inqbus/installer/installer.py
@@ -1,17 +1,15 @@
 from task import TaskMixin
 
+
 class Installer(object):
 
     def __init__(self):
         # dictionary to hold handler
         self.registered_handler = {}
         # list to hold all purposes for which a step can be added
-        self.purposes = ['globalpackages', 'python', 'virtualenv', 'pythonpackages',
-                         'getcurrentproject', 'updatebashrc']
+        self.purposes = ['globalpackages', 'python', 'virtualenv',
+                         'pythonpackages', 'getcurrentproject', 'updatebashrc']
 
-
-
-    
     def install(self, registry_key):
         if registry_key in self.registered_handler:
             install_steps = self.registered_handler[registry_key]
@@ -26,16 +24,14 @@ class Installer(object):
         else:
             print "No steps registered."
 
-
     def deinstall(self):
         pass
 
-
-    def register_handler(self, host, venv, python_version, 
-                         os, versions, handlers):
+    def register(self, host, venv, p_version, os, versions, handlers):
+        """Function to register handler in installing process."""
         if isinstance(versions, list):
             for version in versions:
-                key = host + '_' + python_version + '_' + venv + '_' + os + version
+                key = host + '_' + p_version + '_' + venv + '_' + os + version
                 if key in self.registered_handler:
                     install_steps = self.registered_handler[key]
                 else:
@@ -47,7 +43,7 @@ class Installer(object):
                 for handler, purpose in handlers:
                     install_steps[purpose].append(handler)
         else:
-            key = host + '_' + python_version + '_' + venv + '_' + os + version
+            key = host + '_' + p + '_' + venv + '_' + os + version
             if key in self.registered_handler:
                 install_steps = self.registered_handler[key]
             else:
