fbcbf69391852a423001ef061c6d607933557ef2
Sandra Rum <sandra@tartaros>
2014-01-27 10:46:32 +0100

    pep8

--- src/inqbus/installer/registration.py
+++ src/inqbus/installer/registration.py
@@ -3,41 +3,43 @@ import platform
 from argparse import ArgumentParser
 from fabric.api import env
 
+
 def get_registry_key(args):
     if args.venv_name:
         venv = 'y'
     else:
         venv = 'n'
-    
+
     if args.host_ip and args.host_ip != 'localhost':
         host = 'remote'
         env.host_string = host_ip
     else:
         host = 'localhost'
         env.host_string = '127.0.0.1'
-    
+
     os_name, os_version, os_id = platform.dist()
     os = os_name + os_version
-    
+
     registry_key = host + '_' + args.python + '_' + venv + '_' + os
     return registry_key
+
 
 def parse_arguments():
     parser = ArgumentParser()
     parser.add_argument("-p", "--python",
-                            action="store",
-                            help="select your Python [system|anaconda]",
-                            dest="python", default='system')
-    
+                        action="store",
+                        help="select your Python [system|anaconda]",
+                        dest="python", default='system')
+
     parser.add_argument("-v", "--virtuelenv",
-                            action="store",
-                            help="selct a virtualenv name",
-                            dest="venv_name", default='')
-    
+                        action="store",
+                        help="selct a virtualenv name",
+                        dest="venv_name", default='')
+
     parser.add_argument("-H", "--host",
-                            action="store",
-                            help="select the host_ip",
-                            dest="host_ip", default='')
+                        action="store",
+                        help="select the host_ip",
+                        dest="host_ip", default='')
+
     args = parser.parse_args()
     return args
-    
