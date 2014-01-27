a05718bd5c46ca746c084dfcca8cbc37ffc32ab6
Sandra Rum <sandra@tartaros>
2014-01-27 10:44:05 +0100

    Pep8 and replaced long names by short ones.

--- src/inqbus/installer/test.py
+++ src/inqbus/installer/test.py
@@ -1,42 +1,55 @@
 from installer import Installer
+from task import TaskMixin
 from registration import parse_arguments, get_registry_key
 
+
 # define some Handler who only print some lines
-class VirtualenvHandler(object):
-    
+class Venv(TaskMixin):
+    """Pseudohandler representing the Handler to install the virtual
+    environment"""
+
     def __init__(self, env_name):
         self.env_name = env_name
-    
+
     def install(self):
         print('virtualenv would be created. Name: %s' % self.env_name)
 
-class GlobalPackageLocalHandler(object):
+
+class GLP(TaskMixin):
+    """Pseudohandler representing the Handler to install global packages on
+    the local system"""
 
     def install(self):
         print('Global packages would be installed locally.')
-        
-class GlobalPackageRemoteHandler(object):
-    
+
+
+class GPR(object):
+    """Pseudohandler representing the Handler to install global packages on
+    the remote system"""
+
     def __init__(self, host):
         self.host = host
 
     def install(self):
-        print('Global packages would be remote installed. Host: %s' % self.host)
+        print('Global packages would be remote installed. Host: %s' %
+              self.host)
 
 
 # create a installer
 maininstaller = Installer()
 
-# register Handler
-maininstaller.register_handler('localhost', 'y', 'system', 'debian', ['7.0', '7.1', '7.2', '7.3'],
-                                [(VirtualenvHandler(args.venv_name), 'virtualenv'),
-                                 (GlobalPackageLocalHandler(), 'globalpackages')])
-
-maininstaller.register_handler('remote', 'n', 'anaconda', 'debian', ['7.0', '7.1', '7.2', '7.3'],
-                                [(GlobalPackageRemoteHandler(args.host_ip), 'globalpackages')])
-
 # parse commandline arguments
 args = parse_arguments()
+
+# register Handler
+maininstaller.register('localhost', 'y', 'system', 'debian',
+                        ['7.0', '7.1', '7.2', '7.3'],
+                        [(Venv(args.venv_name), 'virtualenv'),
+                         (GLP(), 'globalpackages')])
+
+maininstaller.register('remote', 'n', 'anaconda', 'debian',
+                       ['7.0', '7.1', '7.2', '7.3'],
+                       [(GPR(args.host_ip), 'globalpackages')])
 
 # get key from commandlinarguments
 registry_key = get_registry_key(args)
