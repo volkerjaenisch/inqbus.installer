a929e2efed14306c0052596e066e79e31cd8aba0
Sandra Rum <sandra@tartaros>
2014-01-27 10:46:53 +0100

    pep8

--- src/inqbus/installer/task.py
+++ src/inqbus/installer/task.py
@@ -8,30 +8,30 @@ class TaskMixin(object):
     def runtask(self, func):
         self.task_file_name = 'fabric_tasks.txt'
         try:
-            if time.time() - os.path.getmtime(self.task_file_name) > 1800 :
+            if time.time() - os.path.getmtime(self.task_file_name) > 1800:
                 self.clear()
-            with open(self.task_file_name, 'r') as _file :
+            with open(self.task_file_name, 'r') as _file:
                 self.finished_tasks = _file.readlines()
-        except OSError :
+        except OSError:
             self.clear()
             self.finished_tasks = []
         if self.finished(func.__class__.__name__):
-            print('Task "' + func.__class__.__name__ + '" is already done! Skipping')
-        else :
+            print('Task ' + func.__class__.__name__ +
+                  'is already done! Skipping')
+        else:
             func.install()
             self.finish(func.__class__.__name__)
-
 
     def task_out(self, task):
         return task + '\n'
 
     def clear(self):
-        with open(self.task_file_name, 'w') as _file :
+        with open(self.task_file_name, 'w') as _file:
             _file.write("")
 
     def finished(self, task):
         return self.task_out(task) in self.finished_tasks
 
     def finish(self, task):
-        with open(self.task_file_name, 'a') as _file :
+        with open(self.task_file_name, 'a') as _file:
             _file.write(self.task_out(task))
