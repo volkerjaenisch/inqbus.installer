ef007071a598f6ea40fb24f1406f6c51734e4448
Sandra Rum <sandra@tartaros>
2014-01-30 11:21:45 +0100

    Added functions to read WORKON_HOME-settings from .bashrc or set it automatically.

--- src/inqbus/installer/registration.py
+++ src/inqbus/installer/registration.py
@@ -2,7 +2,11 @@ import platform
 import os
 
 from argparse import ArgumentParser
+
 from fabric.api import env
+from fabric import api
+from fabric.context_managers import prefix
+from fabric.operations import run
 
 
 def get_registry_key(args):
@@ -20,6 +24,20 @@ def get_registry_key(args):
 
     os_name, os_version, os_id = platform.dist()
     os_key = os_name + os_version
+    
+    if args.python == 'system' and args.venv_name:
+        home = os.path.join('/', 'home', api.env.user)
+        with prefix('cd %s' % home):
+            workon_home = run('cat .bashrc | grep WORKON_HOME')
+        with prefix(workon_home):
+            env.workon_home = run('echo $WORKON_HOME')
+    else:
+        env.workon_home = "~/.virtualenvs"
+    
+    if env.workon_home == '':
+        env.workon_home = "~/.virtualenvs"
+    
+    print env.workon_home
 
     registry_key = host + '_' + args.python + '_' + venv + '_' + os_key
     return registry_key
